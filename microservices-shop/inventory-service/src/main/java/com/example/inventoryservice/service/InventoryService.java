cea324acc78967d6579f3e752ea0e30f1549f03c
Jhon Ferney Aristizabal Sanchez <jfaswis2@gmail.com>
2023-03-21 12:53:15 +0100

    Add implementation Circuit Breaker

--- microservices-shop/inventory-service/src/main/java/com/example/inventoryservice/service/InventoryService.java
+++ microservices-shop/inventory-service/src/main/java/com/example/inventoryservice/service/InventoryService.java
@@ -3,6 +3,8 @@ package com.example.inventoryservice.service;
 import com.example.inventoryservice.dto.InventoryReponse;
 import com.example.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,12 +12,17 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryReponse> isInStock(List<String> skuCode) {
+        log.info("Wait Started");
+        Thread.sleep(10000);//Simular que la llamada tarda mas de 5 segundos
+        log.info("Wait End");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
                     InventoryReponse.builder()
--- microservices-shop/order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ microservices-shop/order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -23,7 +23,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
 
-    public void placeOrder(OrderRequest orderRequest) {
+    public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
@@ -53,6 +53,7 @@ public class OrderService {
         }
 
         orderRepository.save(order);
+        return "Order Placed Successfully";
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
